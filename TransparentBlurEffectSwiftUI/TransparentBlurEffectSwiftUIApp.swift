7a481ef8bba12d73155b0041f7d5e4ba751fc07c
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-06-22 14:39:19 +0300

    Initial Commit

---
+++ TransparentBlurEffectSwiftUI/TransparentBlurEffectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransparentBlurEffectSwiftUIApp.swift
+//  TransparentBlurEffectSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 22.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransparentBlurEffectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
